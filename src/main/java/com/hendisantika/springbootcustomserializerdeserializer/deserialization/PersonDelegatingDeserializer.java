a7c36da0260a66fd9ed17e86bae998d70bdc1a4f
Hendi Santika <hendisantika@gmail.com>
2020-02-24 06:39:01 +0700

    #9 - Add PersonDelegatingDeserializer

--- src/main/java/com/hendisantika/springbootcustomserializerdeserializer/deserialization/PersonDelegatingDeserializer.java
+++ src/main/java/com/hendisantika/springbootcustomserializerdeserializer/deserialization/PersonDelegatingDeserializer.java
@@ -1,5 +1,7 @@
 package com.hendisantika.springbootcustomserializerdeserializer.deserialization;
 
+import com.fasterxml.jackson.databind.deser.std.DelegatingDeserializer;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : springboot-custom-serializer-deserializer
@@ -9,5 +11,9 @@ package com.hendisantika.springbootcustomserializerdeserializer.deserialization;
  * Date: 24/02/20
  * Time: 06.34
  */
-public class PersonDelegatingDeserializer {
+public class PersonDelegatingDeserializer extends DelegatingDeserializer {
+
+    private final PersonRequestCustomDeserializer personRequestCustomDeserializer =
+            new PersonRequestCustomDeserializer();
+
 }
