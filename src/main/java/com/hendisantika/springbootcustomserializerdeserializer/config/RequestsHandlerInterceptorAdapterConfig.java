0c4ee08dcf7d304d88dc23f37ec9bccc3614e99f
Hendi Santika <hendisantika@gmail.com>
2020-02-24 06:32:48 +0700

    #4 - Add RequestsHandlerInterceptorAdapterConfig

---
+++ src/main/java/com/hendisantika/springbootcustomserializerdeserializer/config/RequestsHandlerInterceptorAdapterConfig.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springbootcustomserializerdeserializer.config;
+
+import org.slf4j.MDC;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-custom-serializer-deserializer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/02/20
+ * Time: 06.31
+ */
+@Configuration
+public class RequestsHandlerInterceptorAdapterConfig extends HandlerInterceptorAdapter {
+    public static final String CUSTOM_API = "custom-api";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        MDC.put(CUSTOM_API, request.getHeader(CUSTOM_API));
+        return true;
+    }
+}
