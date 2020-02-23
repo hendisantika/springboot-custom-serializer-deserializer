cb97bc9ae12099e531d02626e99cfb3617c432d4
Hendi Santika <hendisantika@gmail.com>
2020-02-24 06:30:54 +0700

    #3 - Add MvcConfig

---
+++ src/main/java/com/hendisantika/springbootcustomserializerdeserializer/config/MvcConfig.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootcustomserializerdeserializer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-custom-serializer-deserializer
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/02/20
+ * Time: 06.30
+ */
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+}
